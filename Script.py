09788dc2b569819bc565aa82f24b61d6a5958789
Htpmovies <123440825+Htpmovies@users.noreply.github.com>
2023-01-26 09:46:30 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -45,7 +45,7 @@ Here is the help for my COMMANDS."""
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/LazyDeveloper)</code>
+<code>[Button Text](url=https://t.me/Htpmoviesdnk)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
